a7ea19ef0b5611494b2d7a302718d735d30dd919
Ariq Fraser <ariqfraser@gmail.com>
2023-11-04 20:55:18 +0000

    popup styling

--- src/comps/Popup.jsx
+++ src/comps/Popup.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { styled } from 'styled-components';
 import { saveTasks } from '../utils/saveTasks';
+import Button from './Button';
 
 const Wrapper = styled.div`
     position: absolute;
@@ -16,21 +17,43 @@ const Container = styled.div`
     border: 1px solid rgba(0, 0, 0, 0.16);
     border-radius: 0.5rem;
     background-color: #fdfdfd;
-    padding: 2rem;
+    padding: 1rem;
     filter: drop-shadow(0 4px 6px rgba(0, 0, 0, 0.16));
 `;
 
 const Title = styled.p`
+    font-weight: 500;
     user-select: none;
+`;
+
+const Form = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 4rem;
+    gap: 0.5rem;
+
+    & > input {
+        padding: 0.25rem 0.75rem;
+        border-radius: 0.25rem;
+        border: 2px solid rgba(0, 0, 0, 0.14);
+    }
+
+    & > button {
+        background-color: rgb(46, 51, 216);
+        color: white;
+        border: none;
+        cursor: pointer;
+        border-radius: 0.25rem;
+        font-weight: bold;
+    }
 `;
 
 const Popup = ({ setOpen, tasks }) => {
     const taskInputRef = useRef();
-    const [error, setError] = useState('')
+    const [error, setError] = useState('');
 
     const close = () => {
-        setOpen(false)
-    }
+        setOpen(false);
+    };
 
     const addTask = () => {
         const value = taskInputRef.current.value.trim();
@@ -39,24 +62,30 @@ const Popup = ({ setOpen, tasks }) => {
             return;
         }
 
-        tasks.push({title: value, date: new Date(), isActive: true});
-        saveTasks(tasks)
+        tasks.push({ title: value, date: new Date(), isActive: true });
+        saveTasks(tasks);
         close();
-    }
+    };
 
-    const handleKeyUp = ({key}) => {
+    const handleKeyUp = ({ key }) => {
         if (key !== 'Enter') return;
 
         addTask();
-    }
+    };
 
     return (
         <Wrapper onClick={close}>
             <Container onClick={(e) => e.stopPropagation()}>
                 <Title>Add Task</Title>
-                <input type="text" ref={taskInputRef} onKeyUp={handleKeyUp}/>
-                <button onClick={addTask}>Add</button>
-                <br /> {error}
+                <Form>
+                    <input
+                        type="text"
+                        ref={taskInputRef}
+                        onKeyUp={handleKeyUp}
+                    />
+                    <button onClick={addTask}>Add</button>
+                </Form>
+                <span style={{color: 'red'}}>{error}</span>
             </Container>
         </Wrapper>
     );
